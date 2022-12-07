eea99f2bf1c6d7cb84f65d3b890b7b7ecf90d210
Andres Martin <85468387+Apoloservicios@users.noreply.github.com>
2022-12-07 18:24:49 -0300

    vista de todos los celulares con el componente Showphones

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import ShowPhones from "./components/ShowPhones"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ShowPhones/>} />
+        
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
+
