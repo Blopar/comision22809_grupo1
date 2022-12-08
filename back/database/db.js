4bbff4271011936573bba9cdc87382eb2e64b1c4
Pablo Sparhakl <Pablo.Martin.Sparhakl1@ibm.com>
2022-12-08 00:41:23 -0300

    retornado al valor original de la db credencial

--- back/database/db.js
+++ back/database/db.js
@@ -1,7 +1,7 @@
 import  {Sequelize} from "sequelize"
 
 
-const db = new Sequelize ("celulares","root","",{
+const db = new Sequelize ("celulares","root","MySqlR00t@Jmuthuan",{
     host:"localhost",
     dialect:"mysql",
     port:3306
