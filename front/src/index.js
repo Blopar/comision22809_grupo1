d7900e509ff3a37a1ab9c37433812d43f765f74e
jose muthuan <josemuthuan@gmail.com>
2022-12-09 15:14:17 -0300

    creacion de branch backEnd

--- front/src/index.js
+++ front/src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -12,4 +13,20 @@ root.render(
 
     <App />
 
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+//importamos bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <App />
+
+>>>>>>> 12f5f6621f642e78163d5ee57f3d4102008116b7
 );
