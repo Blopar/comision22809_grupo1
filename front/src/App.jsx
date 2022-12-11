1396797de103e78132968c33685388723326d915
Pablo Sparhakl <Pablo.Martin.Sparhakl1@ibm.com>
2022-12-11 01:02:56 -0300

    Se quitaron lineas duplicadas en el front - conflicto de merge

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import "./App.css";
 
 import ShowPhones from "./components/ShowPhones"
@@ -21,29 +21,3 @@ function App() {
 
 export default App;
 
-
-=======
-import "./App.css";
-
-import ShowPhones from "./components/ShowPhones"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-function App() {
-  return (
-    <div className="App">
-      
-      
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ShowPhones/>} />
-        
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
-
-
->>>>>>> 12f5f6621f642e78163d5ee57f3d4102008116b7
